2e19e7818ff35fc0df237973e048b98ff434f28a
Azamat Talap <78661730+tlpazmt@users.noreply.github.com>
2023-04-28 14:39:04 +0600

    fix

--- src/test/java/Test/CrossBrowserTest.java
+++ src/test/java/Test/CrossBrowserTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CrossBrowserTest {
     WebDriver driver;
@@ -14,7 +15,14 @@ public class CrossBrowserTest {
     @Parameters({"browserName"})
     public void test(@Optional("Chrome") String browserName) {
         if (browserName.equals("Chrome")) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized"); // open Browser in maximized mode
+            options.addArguments("disable-infobars"); // disabling infobars
+            options.addArguments("--disable-extensions"); // disabling extensions
+            options.addArguments("--disable-gpu"); // applicable to windows os only
+            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            options.addArguments("--no-sandbox"); // Bypass OS security model
+            driver = new ChromeDriver(options);
         } else if (browserName.equals("Firefox")) {
             driver = new FirefoxDriver();
         }
